f1a47b43245752a6fc8e6f8dbde337c81d9aea5f
LeonardoCG <andresxach7@gmail.com>
2023-01-26 00:28:40 -0600

    Class users v1

---
+++ server/class/usuarios.js
@@ -0,0 +1,54 @@
+
+
+
+class Usuario  {
+    //inicializar en el constructor el arreglo de personas
+    constructor() {
+         this.personas = [];
+    }
+    //metodos
+    agregarPersona(id, persona) {
+
+        let persona = {
+            id, 
+            nombre 
+        }
+        //agregar persona
+        this.personas.push(persona);
+
+        return this.personas;
+    }
+
+    getPersona( id ) {
+        let persona = this.personas.filter( persona => persona.id === id)[0]; // nos devuelve un unico registro [0]
+        
+        return persona;
+    }
+
+    getPersonas() {
+        return this.personas;
+    }
+
+    getPersonasSalas( sala ) {
+        //
+    }
+
+    borrarPersona( id ) {
+
+        let personaBorrada = this.getPersona(id);
+        //Devuelve solo las personas que quedan activas
+        this.personas = this.personas.filter( persona => persona.id != id);
+
+        return personaBorrada;
+    }
+}
+
+
+
+
+
+
+
+module.exports = {
+    Usuarios
+}
